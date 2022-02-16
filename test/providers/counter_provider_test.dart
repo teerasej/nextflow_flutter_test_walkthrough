30f439b66161df7ef78eabd933ef488acd2e3b88
Teerasej Jiraphatchandej <teerasej@gmail.com>
2022-02-16 21:44:27 +0700

    mock a callback function to detect notifylistener()

--- test/providers/counter_provider_test.dart
+++ test/providers/counter_provider_test.dart
@@ -1,19 +1,31 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
 import 'package:nextflow_flutter_test_walkthrough/providers/counter_provider.dart';
+
+class MockCallBackFuntion extends Mock {
+  call();
+}
 
 void main() {
   group('counter provider', () {
+    late CounterProvider provider;
+    final notifyListenerCallback = MockCallBackFuntion();
+
+    setUp(() {
+      provider = CounterProvider()..addListener(notifyListenerCallback);
+      reset(notifyListenerCallback);
+    });
+
     test('initial value is 0', () {
-      var provider = CounterProvider();
       expect(provider.count, 0);
     });
 
     test('increment should add 1 to counter', () {
-      var provider = CounterProvider();
       provider.increment();
       expect(provider.count, 1);
       provider.increment();
       expect(provider.count, 2);
+      verify(notifyListenerCallback()).called(2);
     });
   });
 }
